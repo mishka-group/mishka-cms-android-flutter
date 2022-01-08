fe7143932333c8a471d3243676238fec2876ff6e
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-08 21:17:43 +0330

    improve router

--- lib/apps/enums/route_name.dart
+++
@@ -1,5 +0,0 @@
-/// {@template route_name}
-/// make sure the names you put in enum are the same as
-/// the names of the routes you put in [Routes]
-/// {@endtemplate}
-enum RouteName { signIn, signUp, mishkaBlog, mishkaArticle }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,45 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mishka_cms_android_flutter/apps/mishka_blog_article_schema.dart';
-import 'package:mishka_cms_android_flutter/controllers/mishka_blog/mishka_blog_bloc.dart';
 import 'package:mishka_cms_android_flutter/templates/intro/view/intro.dart';
-import 'package:mishka_cms_android_flutter/templates/mishka_blog/mishka_blog.dart';
+import 'package:mishka_cms_android_flutter/templates/post/view/post.dart';
 import 'package:mishka_cms_android_flutter/templates/sign_in/view/sign_in.dart';
 import 'package:mishka_cms_android_flutter/templates/sing_up/view/sign_up.dart';
+
+import 'controllers/home/home_bloc.dart';
+import 'templates/home/view/home.dart';
+
+class RouterSchema {
+  SelectNavigation nav;
+  dynamic arguments;
+  RouterSchema({required this.nav, this.arguments});
+}
 
 class Routes {
   Route? onGenerateRoute(RouteSettings routeSettings) {
     // var data = routeSettings.arguments as String;
-    switch (routeSettings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) {
+    MaterialPageRoute(builder: (_) {
+      switch (routeSettings.name) {
+        case '/':
           return const Intro();
-        });
-      case '/signIn':
-        return MaterialPageRoute(builder: (_) {
+
+        case '/signIn':
           return const SignIn();
-        });
-      case '/signUp':
-        return MaterialPageRoute(builder: (_) {
+        case '/signUp':
           return const SignUp();
-        });
-      case '/mishkaBlog':
-        return MaterialPageRoute(builder: (_) {
-          return const MishkaBlog(articles: [
-            MishkaBlogArticle('title', 'author', 'timeToRead', 'img')
-          ]);
-        });
-      case '/mishkaArticle':
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: MishkaBlogBloc(),
-            child: const MishkaArticle(
-                article:
-                    MishkaBlogArticle('title', 'author', 'timeToRead', 'img')),
-          ),
-        );
-      default:
-        return null;
-    }
+        case '/home':
+          return BlocProvider.value(
+            value: HomeBloc(),
+            child: const Home(),
+          );
+        case '/post':
+          return BlocProvider.value(
+            value: HomeBloc(),
+            child: const Post(),
+          );
+        default:
+          return const Home();
+      }
+    });
   }
 }
+
+class AppRouter {
+  AppRouter(this.context);
+  final BuildContext context;
+
+  getRoot() {
+    Navigator.of(context).pushNamed('/');
+  }
+
+  getTo(RouterSchema router) {
+    Navigator.of(context)
+        .pushNamed('/' + router.nav.name, arguments: router.arguments);
+  }
+
+  getUntilTo(RouterSchema router) {
+    Navigator.of(context).pushNamedAndRemoveUntil(
+        '/' + router.nav.name, (route) => false,
+        arguments: router.arguments);
+  }
+
+  pop() {
+    Navigator.of(context).pop();
+  }
+}
+
+/// {@template route_name}
+/// make sure the names you put in enum are the same as
+/// the names of the routes you put in [Routes]
+/// {@endtemplate}
+enum SelectNavigation { signIn, signUp, home, post }
