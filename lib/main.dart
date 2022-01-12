3ae0fce75353789d42daddc02ab289ba8d1db573
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-12 20:44:31 +0330

    project information added (maintainers, licenses, links)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-// Copyright (c) 2021, Mishka Group (Trangell)
-// https://trangell.com/
-//
-// Use of this source code is governed by an Apache-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/Apache-2.0.
 //
 //  main.dart
-//  mishka-cms-android-flutter
-//
-//  Created by Husen on 2022 09 Jan.
+//  Created on 2022 09 Jan.
+//  Copyright © Mishka Group Flutter application,
+//  Developed by 2021 Hossein HassanNejad.
 //
 
 import 'package:mishka_cms_android_flutter/routes.dart';
