c15fccfa3f134e0de636a63768d41a61dc7aab27
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-16 22:46:18 +0330

    add basic router test

---
+++ test/routers_test.dart
@@ -0,0 +1,12 @@
+//
+//  routes_test.dart
+//  Created on 2022 15 January.
+//  Copyright © Mishka Group Flutter application,
+//  Developed by 2021 Hossein HassanNejad.
+//
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Routers', () {});
+}
