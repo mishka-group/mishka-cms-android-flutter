14004f4ac0922936b5312ae7f768a881e5d9d3f5
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-24 19:51:40 +0330

    add basic sign_up screen

--- lib/apps/helper/router_schema.dart
+++ lib/apps/helper/router_schema.dart
@@ -9,9 +9,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:mishka_cms_android_flutter/templates/blog/view/blog.dart';
 import 'package:mishka_cms_android_flutter/templates/home/home.dart';
-import 'package:mishka_cms_android_flutter/templates/post/view/post.dart';
-import 'package:mishka_cms_android_flutter/templates/sign_in/view/sign_in.dart';
-import 'package:mishka_cms_android_flutter/templates/sing_up/view/sign_up.dart';
+import 'package:mishka_cms_android_flutter/templates/post/post.dart';
+import 'package:mishka_cms_android_flutter/templates/sign_in/sign_in.dart';
+import 'package:mishka_cms_android_flutter/templates/sign_up/sign_up.dart';
 
 class RouterSchema {
   UniqueKey id = UniqueKey();
---
+++ lib/templates/sign_up/view/sign_up.dart
@@ -0,0 +1,17 @@
+//
+//  sign_up.dart
+//  Created on 2022 24 Jan.
+//  Copyright © Mishka Group Flutter application,
+//  Developed by 2021 Hossein HassanNejad.
+//
+
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
