41e909b165a24d546b3ba63c0461744e18215127
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-24 19:40:13 +0330

    add basic blog screen

--- lib/apps/helper/router_schema.dart
+++ lib/apps/helper/router_schema.dart
@@ -8,7 +8,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:mishka_cms_android_flutter/controllers/home/home_bloc.dart';
-import 'package:mishka_cms_android_flutter/templates/blog/blog.dart';
+import 'package:mishka_cms_android_flutter/templates/blog/view/blog.dart';
 import 'package:mishka_cms_android_flutter/templates/home/home.dart';
 import 'package:mishka_cms_android_flutter/templates/post/view/post.dart';
 import 'package:mishka_cms_android_flutter/templates/sign_in/view/sign_in.dart';
