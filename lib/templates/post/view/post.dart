e76a756a60847d79538f98e0478ceeaf1ed175db
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-24 19:44:09 +0330

    add basic post screen

---
+++ lib/templates/post/view/post.dart
@@ -0,0 +1,17 @@
+//
+//  post.dart
+//  Created on 2022 24 Jan.
+//  Copyright © Mishka Group Flutter application,
+//  Developed by 2021 Hossein HassanNejad.
+//
+
+import 'package:flutter/material.dart';
+
+class Post extends StatelessWidget {
+  const Post({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
