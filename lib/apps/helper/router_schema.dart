8738bac58421fc787937665017aa1078ccce43cd
Hossein HassanNejad <hosseinspell@gmail.com>
2022-01-24 19:41:48 +0330

    add basic home screen

--- lib/apps/helper/router_schema.dart
+++ lib/apps/helper/router_schema.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:mishka_cms_android_flutter/controllers/home/home_bloc.dart';
 import 'package:mishka_cms_android_flutter/templates/blog/view/blog.dart';
 import 'package:mishka_cms_android_flutter/templates/home/home.dart';
 import 'package:mishka_cms_android_flutter/templates/post/view/post.dart';
---
+++ lib/templates/home/view/home.dart
@@ -0,0 +1,17 @@
+//
+//  home.dart
+//  Created on 2022 24 Jan.
+//  Copyright © Mishka Group Flutter application,
+//  Developed by 2021 Hossein HassanNejad.
+//
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
