2453c352f84251b00a740a304cc3f15352d700e2
Hossein HassanNejad <hosseinspell@gmail.com>
2022-02-07 01:30:48 +0330

    added doc of RouterSchema

--- lib/apps/helper/router_schema.dart
+++ lib/apps/helper/router_schema.dart
@@ -13,6 +13,32 @@ import 'package:mishka_cms_android_flutter/templates/post/post.dart';
 import 'package:mishka_cms_android_flutter/templates/sign_in/sign_in.dart';
 import 'package:mishka_cms_android_flutter/templates/sign_up/sign_up.dart';
 
+///
+/// [RouterSchema] is a model to implement all Mishka CMS app routes.
+///
+/// [id]: uniq id for each route.
+///
+/// [routeName]: must be a specific [SelectRoute] name for the current route.
+///
+/// [route]: in this callback, you get an [argument] that if you want to send
+/// data from one router to another, you must pass this [argument] to a router
+/// class. and finally you have to return a router(from template).
+///
+/// [blocProviderValue]: if your route is going to use bloc, we will put that
+/// bloc in this section for the current route and your route will look like
+/// this in the background:
+/// ```dart
+/// BlocProvider.value(
+///   value: blocProviderValue,
+///   child: route,
+/// );
+/// ```
+///
+/// > if u want to add new [bloc] or other statemanager libraries wrapper:
+/// > 1. add a new optional param to [RouterSchema] just like `blocProviderValue`
+/// > 2. read full documentation of [Routes]
+///
+
 class RouterSchema {
   UniqueKey id = UniqueKey();
   SelectRoute routeName;
@@ -25,9 +51,6 @@ class RouterSchema {
 
 SelectRoute initialRoute = SelectRoute.home;
 
-///
-/// Basic Doc for starting.
-///
 var routers = [
   RouterSchema(routeName: SelectRoute.blog, route: (argument) => const Blog()),
   RouterSchema(
